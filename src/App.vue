4aeb08ebe4a9d90f47680bed26fbde6df1a94902
GianluGiarde <gianlugiarde@gmail.com>
2024-02-06 03:19:53 +0100

    Add background

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,19 @@
 <script></script>
 
 <template>
-  <router-view></router-view>
+  <main class="h-100" style="padding: 6rem 0rem">
+    <router-view></router-view>
+  </main>
 </template>
 
 <style lang="scss">
 @use "./assets/styles/general.scss" as *;
+
+body {
+  height: 100vh;
+  background-image: url('/app-bg.jpeg');
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+}
 </style>
